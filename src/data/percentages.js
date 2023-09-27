4d56211f8a29551d9181b96dac7f82d1e8491826
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-09-27 20:36:49 -0300

    feat: creating percentages db

---
+++ src/data/percentages.js
@@ -0,0 +1,22 @@
+export const percentages = [
+  {
+    value: 5,
+    id: 1,
+  },
+  {
+    value: 10,
+    id: 2,
+  },
+  {
+    value: 15,
+    id: 3,
+  },
+  {
+    value: 25,
+    id: 4,
+  },
+  {
+    value: 50,
+    id: 5,
+  },
+];
