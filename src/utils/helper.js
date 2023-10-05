a926ac80e4ca230c160a2e497497fc3031001360
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-04 23:46:43 -0300

    refac: testing helper function

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,26 +1,26 @@
-export function validInput(e, setState) {
+export const validInput = (e, setState) => {
   const value = e.target.value;
 
   const numericValue = value.replace(/[^0-9.]/g, "");
 
-  if (value === numericValue) {
-    setState(value);
-  } else {
-    alert("error");
-  }
-}
+  // if (value !== numericValue || value <= 0) {
 
-export function calculate(
+  // } else {
+  //   setState(value);
+  // }
+};
+
+export const calculate = (
   billValue,
   peopleNumber,
   tipValue,
   setBillValue,
   setTipValue
-) {
+) => {
   const total = billValue / peopleNumber;
   const tipAmount = (total * tipValue) / 100;
   const totalAmount = total + tipAmount;
 
   setBillValue(totalAmount.toFixed(2));
   setTipValue(tipAmount.toFixed(2));
-}
+};
