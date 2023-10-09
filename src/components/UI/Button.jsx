f00a2891910bd53ab693aab976403d83305dd856
wagnnermorais <wagnermoraiscnt@gmail.com>
2023-10-08 21:35:16 -0300

    style: adding class button

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ import "../../styles/Components/Button.css";
 
 const Button = ({ type, onClick, text }) => {
   return (
-    <button className="button-disabled" type={type} onClick={onClick}>
+    <button className="button" type={type} onClick={onClick}>
       {text}
     </button>
   );
